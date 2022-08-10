def27385945df56035837500a9a7f679a0d18186
angelsanchezh <angelsanchezh@gmail.com>
2022-08-10 18:16:24 -0500

    tareas1

---
+++ codigotarea.js
@@ -0,0 +1,5 @@
+function callMenu () {
+
+let nro_ejercicio = parseInt(prompt("escoge el numero de ejercicio que quieres ejecutar: \r\n 1.suma. \r\n 2.Promedio de Examanes \r\n 3. Calcular el area del rectangulo \r\n 4.Calcular el area del triangulo \r\n 5.calcular el area del Circulo \r\n 6. Calcular salario semanal \r\n 7. convertir pulgadas \r\n 8. cambiar a dolaresescoge una opcion \r\n 9.Empresa contrata personal \r\n 10. Nombre y edad de 3 personas \r\n 11. Bono por antiguedad \r\n 12. Incremento salario profesor \r\n 13. Califiaciones de alumnnos \r\n 14. Contabilizar lote de N focos \r\n 15. Determinar si persoan puede votar "))
+    
+}
